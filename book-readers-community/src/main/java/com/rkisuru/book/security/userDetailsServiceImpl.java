ef8c561eb87e9c620722f273e70d4d937bca5c9a
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-14 16:30:59 +0530

    create class userDetailsServiceImpl

---
+++ book-readers-community/src/main/java/com/rkisuru/book/security/userDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.rkisuru.book.security;
+
+import com.rkisuru.book.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class userDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByEmail(userEmail)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+    }
+}
