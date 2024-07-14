4bd831128a46372798349b11984cd665191f5164
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-14 14:15:42 +0530

    create Token class

---
+++ book-readers-community/src/main/java/com/rkisuru/book/user/Token.java
@@ -0,0 +1,29 @@
+package com.rkisuru.book.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
