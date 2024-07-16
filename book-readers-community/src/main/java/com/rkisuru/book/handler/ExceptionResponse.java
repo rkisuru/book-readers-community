2ff02be6994cdb7146070d066d703c92ee65cd35
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-16 14:37:22 +0530

    feat: implement GlobalExceptionHandler class to handle exceptions thrown by controllers

---
+++ book-readers-community/src/main/java/com/rkisuru/book/handler/ExceptionResponse.java
@@ -0,0 +1,24 @@
+package com.rkisuru.book.handler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+import java.util.Set;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ExceptionResponse {
+
+    private Integer businessErrorCode;
+    private String businessErrorDescription;
+    private String error;
+    private Set<String> validationErrors;
+    private Map<String, String> errors;
+}
