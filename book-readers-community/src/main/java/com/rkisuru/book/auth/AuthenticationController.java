dfac783bae33bd5eb4d714f2b7c1b0c0a59407ec
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-16 12:24:47 +0530

    feat: implement authentication and user account activation

--- book-readers-community/src/main/java/com/rkisuru/book/auth/AuthenticationController.java
+++ book-readers-community/src/main/java/com/rkisuru/book/auth/AuthenticationController.java
@@ -21,6 +21,20 @@ public class AuthenticationController {
     public ResponseEntity<?> register(@RequestBody @Valid RegistrationRequest request) throws MessagingException {
         authenticationService.register(request);
         return ResponseEntity.accepted().build();
-
     }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody @Valid AuthenticationRequest request
+    ){
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+    @GetMapping("/activate-account")
+    public void confirm(
+            @RequestParam String token
+    ) throws MessagingException {
+        authenticationService.activateAccount(token);
+    }
+
 }
