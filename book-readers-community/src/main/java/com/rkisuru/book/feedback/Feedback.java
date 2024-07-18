caa59e23c82111c23f36d1b91adf11590bd9a2fd
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-18 10:19:37 +0530

    create Feedback class

---
+++ book-readers-community/src/main/java/com/rkisuru/book/feedback/Feedback.java
@@ -0,0 +1,38 @@
+package com.rkisuru.book.feedback;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Feedback {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private Double note;
+    private String comment;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedAt;
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private Integer createdBy;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer lastModifiedBy;
+}
