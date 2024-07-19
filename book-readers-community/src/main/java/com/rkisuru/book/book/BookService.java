c7286fa7c85c7bca67bf3db3dbdfacfc489d14f5
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-19 11:22:33 +0530

    implement save Book method

---
+++ book-readers-community/src/main/java/com/rkisuru/book/book/BookService.java
@@ -0,0 +1,25 @@
+package com.rkisuru.book.book;
+
+import com.rkisuru.book.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookMapper bookMapper;
+
+    private final BookRepository bookRepository;
+
+    public Integer save(BookRequest request, Authentication connectedUser) {
+
+        User user = ((User) connectedUser.getPrincipal());
+        Book book = bookMapper.toBook(request);
+        book.setOwner(user);
+        return bookRepository.save(book).getId();
+    }
+
+
+}
