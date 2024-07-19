d93bd467a4f0d29508588e0016fc71c12d07e200
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-19 11:53:34 +0530

    implement getRate method

--- book-readers-community/src/main/java/com/rkisuru/book/book/Book.java
+++ book-readers-community/src/main/java/com/rkisuru/book/book/Book.java
@@ -36,4 +36,17 @@ public class Book extends BaseEntity {
     @OneToMany(mappedBy = "book")
     private List<BookTransactionHistory> transactionHistories;
 
+    @Transient
+    public double getRate(){
+        if (feedbacks == null || feedbacks.isEmpty()){
+            return 0.0;
+        }
+        var rate = this.feedbacks.stream()
+                .mapToDouble(Feedback::getNote)
+                .average()
+                .orElse(0.0);
+        double roundedRate = Math.round(rate*10.0)/10.0;
+        return roundedRate;
+    }
+
 }
