a364ffd6e224a57c35b60cc67bdfd78d0c512522
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-19 11:24:01 +0530

    feat: implement saveBook endpoint

---
+++ book-readers-community/src/main/java/com/rkisuru/book/book/BookController.java
@@ -0,0 +1,26 @@
+package com.rkisuru.book.book;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("books")
+@RequiredArgsConstructor
+@Tag(name = "book")
+public class BookController {
+
+    private final BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<Integer> saveBook(
+            @Valid @RequestBody BookRequest request,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(bookService.save(request, connectedUser));
+    }
+
+}
