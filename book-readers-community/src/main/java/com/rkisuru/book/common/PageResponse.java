f53294f725e6b459b31eaf309d47cdec838a700c
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-20 09:54:22 +0530

    create PageResponse class

---
+++ book-readers-community/src/main/java/com/rkisuru/book/common/PageResponse.java
@@ -0,0 +1,23 @@
+package com.rkisuru.book.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResponse<T> {
+
+    private List<T> content;
+    private int number;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+    private boolean first;
+    private boolean last;
+}
