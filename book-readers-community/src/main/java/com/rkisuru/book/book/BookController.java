bdd62fed34e185b816f3881b72b81751df58882d
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-20 10:35:39 +0530

    feat: implement findAllBooksByOwner endpoint

--- book-readers-community/src/main/java/com/rkisuru/book/book/BookController.java
+++ book-readers-community/src/main/java/com/rkisuru/book/book/BookController.java
@@ -39,4 +39,14 @@ public class BookController {
     ) {
         return ResponseEntity.ok(bookService.findAllBooks(page, size, connectedUser));
     }
+
+    @GetMapping("/owner")
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooksByOwner(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    ){
+        return ResponseEntity.ok(bookService.findAllBooksByOwner(page, size, connectedUser));
+    }
+
 }
