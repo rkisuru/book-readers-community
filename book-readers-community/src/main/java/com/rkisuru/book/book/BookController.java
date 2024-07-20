c99f94993f87536b1db892dc9d9fbcb5f1fb9c59
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-20 09:56:54 +0530

    implement findAllBooks endpoint

--- book-readers-community/src/main/java/com/rkisuru/book/book/BookController.java
+++ book-readers-community/src/main/java/com/rkisuru/book/book/BookController.java
@@ -1,5 +1,6 @@
 package com.rkisuru.book.book;
 
+import com.rkisuru.book.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -28,5 +29,14 @@ public class BookController {
             @PathVariable Integer bookId
     ){
         return ResponseEntity.ok(bookService.findById(bookId));
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooks(
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(bookService.findAllBooks(page, size, connectedUser));
     }
 }
