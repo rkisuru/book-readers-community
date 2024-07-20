aa0197092667c5f51755f224033b46bb42bc1e72
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-20 14:07:46 +0530

    implement findAllReturnedBooks abstract method

--- book-readers-community/src/main/java/com/rkisuru/book/history/BookTransactionHistoryRepository.java
+++ book-readers-community/src/main/java/com/rkisuru/book/history/BookTransactionHistoryRepository.java
@@ -9,8 +9,15 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
 
     @Query("""
             SELECT history
-            FROM BookTransactionHistory
+            FROM BookTransactionHistory history
             WHERE history.user.id = :userId
             """)
     Page<BookTransactionHistory> findAllBorrowedBooks(Pageable pageable, Integer userId);
+
+    @Query("""
+            SELECT history
+            FROM BookTransactionHistory history
+            WHERE history.book.owner.id = :userId
+            """)
+    Page<BookTransactionHistory> findAllReturnedBooks(Pageable pageable, Integer userId);
 }
