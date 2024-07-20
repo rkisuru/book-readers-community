2e31c7c4325789c4977dcaee09c8842be6a56537
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-20 09:55:56 +0530

    implement findAllDisplayableBooks method

--- book-readers-community/src/main/java/com/rkisuru/book/book/BookRepository.java
+++ book-readers-community/src/main/java/com/rkisuru/book/book/BookRepository.java
@@ -1,7 +1,18 @@
 package com.rkisuru.book.book;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
 
+    @Query("""
+            SELECT book
+            FROM BOOK book
+            WHERE book.archived = false
+            AND book.shareable = true
+            AND book.owner.id != :userId
+            """)
+    Page<Book> findAllDisplayableBooks(Pageable pageable, Integer userId);
 }
