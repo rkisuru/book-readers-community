d9667080a732c41403fbf1462ace0ebd49b7ef94
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-22 13:26:31 +0530

    implement findByBookIdAndOwnerId abstract method

--- book-readers-community/src/main/java/com/rkisuru/book/history/BookTransactionHistoryRepository.java
+++ book-readers-community/src/main/java/com/rkisuru/book/history/BookTransactionHistoryRepository.java
@@ -36,10 +36,20 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
     @Query("""
             SELECT transaction
             FROM BookTransactionHistory transaction
-            WHERE transaction.user.Id = :userId
+            WHERE transaction.user.id = :userId
             AND transaction.book.id = :bookId
             AND transaction.returned = false
             AND transaction.returnApproved = false
             """)
     Optional<BookTransactionHistory> findByBookIdAndUserId(Integer bookId, Integer userId);
+
+    @Query("""
+    SELECT transaction
+    FROM BookTransactionHistory transaction
+    WHERE transaction.book.owner.id = :userId
+    AND transaction.book.id = :bookId
+    AND transaction.returned = true
+    AND transaction.returnApproved = false
+    """)
+    Optional<BookTransactionHistory> findByBookIdAndOwnerId(Integer bookId, Integer id);
 }
