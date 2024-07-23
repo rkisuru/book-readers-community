f74993abfb7272b8fa62ca73a6ecb2c834c11b77
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-23 22:29:43 +0530

    implement FeedbackMapper class

---
+++ book-readers-community/src/main/java/com/rkisuru/book/feedback/FeedbackMapper.java
@@ -0,0 +1,22 @@
+package com.rkisuru.book.feedback;
+
+import com.rkisuru.book.book.Book;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FeedbackMapper {
+
+    public Feedback toFeedback(FeedbackRequest feedbackRequest) {
+
+        return Feedback.builder()
+                .note(feedbackRequest.note())
+                .comment(feedbackRequest.comment())
+                .book(Book.builder()
+                        .id(feedbackRequest.bookId())
+                        .archived(false)
+                        .shareable(false)
+                        .build()
+                )
+                .build();
+    }
+}
