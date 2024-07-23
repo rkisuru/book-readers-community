00421e135fc1fcae0bc28e52af843231fab4ff1d
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-23 21:34:02 +0530

    implement FileStorageService

---
+++ book-readers-community/src/main/java/com/rkisuru/book/file/FileStorageService.java
@@ -0,0 +1,68 @@
+package com.rkisuru.book.file;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class FileStorageService {
+
+    @Value("${file.upload.photos-output-path}")
+    private String fileUploadPath;
+
+    public Object saveFile(
+            @NonNull MultipartFile sourceFile,
+            @NonNull Integer userId) {
+
+        final String fileUploadSubPath = "users" + File.separator + userId;
+        return uploadFile(sourceFile, fileUploadSubPath);
+    }
+
+    private Object uploadFile(
+            @NonNull MultipartFile sourceFile,
+            @NonNull String fileUploadSubPath) {
+
+        final String finalUploadPath = fileUploadPath + File.separator + fileUploadSubPath;
+        File targetFolder = new File(finalUploadPath);
+        if (!targetFolder.exists()) {
+            boolean folderCreated = targetFolder.mkdirs();
+            if (!folderCreated) {
+                log.warn("Failed to create target folder");
+                return null;
+            }
+        }
+        final String filExtension = getFileExtension(sourceFile.getOriginalFilename());
+        String targetFilePath = finalUploadPath + File.separator + System.currentTimeMillis() + "." + filExtension;
+        Path targetPath = Paths.get(targetFilePath);
+        try {
+            Files.write(targetPath, sourceFile.getBytes());
+            log.info("Successfully uploaded  to {}", targetFilePath);
+            return targetFilePath;
+        } catch (IOException e) {
+            log.error("Failed to write file", e);
+        }
+        return null;
+    }
+
+    private String getFileExtension(String filename) {
+        if (filename == null || filename.isEmpty()) {
+            return "";
+        }
+        int lastDotIndex = filename.lastIndexOf(".");
+        if (lastDotIndex == -1) {
+            return "";
+        }
+        return filename.substring(lastDotIndex + 1).toLowerCase();
+    }
+}
