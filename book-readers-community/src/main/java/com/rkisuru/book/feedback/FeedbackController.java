3e0fbc436b79142ae6050212a1d550cfb71c6ad6
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-23 23:16:28 +0530

    fix: implement findAllFeedbacksByBook endpoint

--- book-readers-community/src/main/java/com/rkisuru/book/feedback/FeedbackController.java
+++ book-readers-community/src/main/java/com/rkisuru/book/feedback/FeedbackController.java
@@ -1,14 +1,12 @@
 package com.rkisuru.book.feedback;
 
+import com.rkisuru.book.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("feedbacks")
@@ -23,5 +21,15 @@ public class FeedbackController {
             @Valid @RequestBody FeedbackRequest feedbackRequest,
             Authentication connectedUser) {
         return ResponseEntity.ok(feedbackService.save(feedbackRequest, connectedUser));
+    }
+
+    @GetMapping("/book/{book-id}")
+    public ResponseEntity<PageResponse<FeedbackResponse>> findAllFeadbacksByBook(
+            @PathVariable("book-id") Integer bookId,
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser)
+    {
+        return ResponseEntity.ok(feedbackService.findAllFeedbacksByBook(bookId, page, size,connectedUser));
     }
 }
