44076a00e889f1357e7a0698f73b27b884bb6b79
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-23 22:29:10 +0530

    implement FeedbackRepository

---
+++ book-readers-community/src/main/java/com/rkisuru/book/feedback/FeedbackRepository.java
@@ -0,0 +1,6 @@
+package com.rkisuru.book.feedback;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedbackRepository extends JpaRepository<Feedback, Integer> {
+}
