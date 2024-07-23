e321a96cad88bb2d58730e02aeab8f1e2a57bbc0
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-23 23:33:38 +0530

    feat: implement OpenApi documentation

---
+++ book-readers-community/src/main/java/com/rkisuru/book/config/OpenApiConfig.java
@@ -0,0 +1,47 @@
+package com.rkisuru.book.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "rkisuru",
+                        email = "iaravinda33@gmail.com"
+                ),
+                description = "OpenApi documentation fot Spring security",
+                title = "OpenApi specification - rkisuru",
+                version = "1,0",
+                license = @License(
+                        name = "Licence"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8888/api/v1"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
