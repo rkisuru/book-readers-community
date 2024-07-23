684998527ede6645c4236d3cc7034042b1e05ad8
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-23 22:55:17 +0530

    feat: implement saveFeedback endpoint

---
+++ book-readers-community/src/main/java/com/rkisuru/book/feedback/FeedbackController.java
@@ -0,0 +1,27 @@
+package com.rkisuru.book.feedback;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("feedbacks")
+@RequiredArgsConstructor
+@Tag(name = "Feedback")
+public class FeedbackController {
+
+    private final FeedbackService feedbackService;
+
+    @PostMapping
+    public ResponseEntity<Integer> saveFeedback(
+            @Valid @RequestBody FeedbackRequest feedbackRequest,
+            Authentication connectedUser) {
+        return ResponseEntity.ok(feedbackService.save(feedbackRequest, connectedUser));
+    }
+}
