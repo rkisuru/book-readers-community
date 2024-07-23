8db15f97398eb3622a298fb30fb4f7935d51a674
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-23 22:55:43 +0530

    implement FeedbackResponse

---
+++ book-readers-community/src/main/java/com/rkisuru/book/feedback/FeedbackResponse.java
@@ -0,0 +1,17 @@
+package com.rkisuru.book.feedback;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FeedbackResponse {
+
+    private Double note;
+    private String comment;
+    private boolean ownFeedback;
+}
