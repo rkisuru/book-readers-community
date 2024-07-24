3cfd982368ef53f18f905253b77f90befcc2cb11
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-24 18:23:04 +0530

    fix: userId to user_id

--- book-readers-community/src/main/java/com/rkisuru/book/user/Token.java
+++ book-readers-community/src/main/java/com/rkisuru/book/user/Token.java
@@ -24,6 +24,6 @@ public class Token {
     private LocalDateTime validatedAt;
 
     @ManyToOne
-    @JoinColumn(name = "userId", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 }
--- book-readers-community/src/main/java/com/rkisuru/book/user/User.java
+++ book-readers-community/src/main/java/com/rkisuru/book/user/User.java
@@ -66,7 +66,7 @@ public class User implements UserDetails, Principal {
 
     @Override
     public String getName() {
-        return "";
+        return email;
     }
 
     @Override
