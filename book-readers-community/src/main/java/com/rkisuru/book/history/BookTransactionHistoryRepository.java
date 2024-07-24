591499a349fce831edbd30a71cf1493131c335c5
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-24 20:15:02 +0530

    fix: rename user_id, book_id

--- book-readers-community/src/main/java/com/rkisuru/book/history/BookTransactionHistoryRepository.java
+++ book-readers-community/src/main/java/com/rkisuru/book/history/BookTransactionHistoryRepository.java
@@ -12,44 +12,44 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
     @Query("""
             SELECT history
             FROM BookTransactionHistory history
-            WHERE history.user.id = :userId
+            WHERE history.user.id = :user_id
             """)
-    Page<BookTransactionHistory> findAllBorrowedBooks(Pageable pageable, Integer userId);
+    Page<BookTransactionHistory> findAllBorrowedBooks(Pageable pageable, Integer user_id);
 
     @Query("""
             SELECT history
             FROM BookTransactionHistory history
-            WHERE history.book.owner.id = :userId
+            WHERE history.book.owner.id = :user_id
             """)
-    Page<BookTransactionHistory> findAllReturnedBooks(Pageable pageable, Integer userId);
+    Page<BookTransactionHistory> findAllReturnedBooks(Pageable pageable, Integer user_id);
 
     @Query("""
             SELECT
             (COUNT(*) > 0) AS isBorrowed
             FROM BookTransactionHistory bookTransactionHistory
-            WHERE bookTransactionHistory.user.id = :userId
-            AND bookTransactionHistory.book.id = :bookId
+            WHERE bookTransactionHistory.user.id = :user_id
+            AND bookTransactionHistory.book.id = :book_id
             AND bookTransactionHistory.returnApproved = false
             """)
-    boolean isAlreadyBorrowed(Integer bookId, Integer id);
+    boolean isAlreadyBorrowed(Integer book_id, Integer user_id);
 
     @Query("""
             SELECT transaction
             FROM BookTransactionHistory transaction
-            WHERE transaction.user.id = :userId
-            AND transaction.book.id = :bookId
+            WHERE transaction.user.id = :user_id
+            AND transaction.book.id = :book_id
             AND transaction.returned = false
             AND transaction.returnApproved = false
             """)
-    Optional<BookTransactionHistory> findByBookIdAndUserId(Integer bookId, Integer userId);
+    Optional<BookTransactionHistory> findByBookIdAndUserId(Integer book_id, Integer user_id);
 
     @Query("""
     SELECT transaction
     FROM BookTransactionHistory transaction
-    WHERE transaction.book.owner.id = :userId
-    AND transaction.book.id = :bookId
+    WHERE transaction.book.owner.id = :user_id
+    AND transaction.book.id = :book_id
     AND transaction.returned = true
     AND transaction.returnApproved = false
     """)
-    Optional<BookTransactionHistory> findByBookIdAndOwnerId(Integer bookId, Integer id);
+    Optional<BookTransactionHistory> findByBookIdAndOwnerId(Integer book_id, Integer user_id);
 }
