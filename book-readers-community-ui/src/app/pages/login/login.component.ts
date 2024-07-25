796272b10a36f014f948fa3978afc6f844fb6703
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-25 13:47:58 +0530

    feat: implement login api call

--- book-readers-community-ui/src/app/pages/login/login.component.ts
+++ book-readers-community-ui/src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {AuthenticationRequest} from "../../services/models/authentication-request";
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../services/services';
+import {TokenService} from "../../services/token/token.service";
 
 @Component({
   selector: 'app-login',
@@ -16,6 +17,7 @@ export class LoginComponent {
   constructor(
     private router:Router,
     private authService: AuthenticationService,
+    private tokenService: TokenService
 
   ) {
 
@@ -27,7 +29,7 @@ export class LoginComponent {
       body: this.authRequest
     }).subscribe({
       next: (res)=> {
-
+        this.tokenService.token = res.token as string;
         this.router.navigate(['books']);
       },
       error: (err)=> {
