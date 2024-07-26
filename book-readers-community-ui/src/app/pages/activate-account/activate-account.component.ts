86d50ab67a765b5d37f717e91259a210f0fa17b0
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-26 17:29:15 +0530

    initialize activate-account component

---
+++ book-readers-community-ui/src/app/pages/activate-account/activate-account.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activate-account',
+  templateUrl: './activate-account.component.html',
+  styleUrl: './activate-account.component.scss'
+})
+export class ActivateAccountComponent {
+
+}
