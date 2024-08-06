28ad50787991e853745d9c45b476f4ea7490228b
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-08-06 20:50:53 +0530

    refactor: import HttpClientModule

--- book-readers-community-ui/src/app/app.module.ts
+++ book-readers-community-ui/src/app/app.module.ts
@@ -3,29 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {provideHttpClient} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from "@angular/common/http";
 import { LoginComponent } from './pages/login/login.component';
 import { FormsModule } from '@angular/forms';
-import { TestComponent } from './test/test.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 import {CodeInputModule} from "angular-code-input";
+import { HttpTokenInterceptor } from './services/interceptor/http-token.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    TestComponent,
     RegisterComponent,
     ActivateAccountComponent
   ],
     imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        CodeInputModule
+      BrowserModule,
+      HttpClientModule,
+      AppRoutingModule,
+      CodeInputModule,
+      FormsModule
     ],
-  providers: [provideHttpClient()],
+  providers: [
+    HttpClient,
+     {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpTokenInterceptor,
+      multi: true
+     }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
