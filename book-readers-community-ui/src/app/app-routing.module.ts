1ff623a5cb6146222a45b5ebccf2d3ad62e0734c
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-08-06 20:46:58 +0530

    remove TestComponent routing

--- book-readers-community-ui/src/app/app-routing.module.ts
+++ book-readers-community-ui/src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { TestComponent } from './test/test.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 
 const routes: Routes = [
   {
     path:'login', component:LoginComponent
-  },
-  {
-    path:'test', component:TestComponent
   },
   {
     path:'register', component:RegisterComponent
