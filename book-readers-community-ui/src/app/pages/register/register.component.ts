e115b1024d9c55dbf7f512f8eb220fd6bb842ddf
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-08-06 20:45:52 +0530

    refactor

--- book-readers-community-ui/src/app/pages/register/register.component.ts
+++ book-readers-community-ui/src/app/pages/register/register.component.ts
@@ -1,44 +1,40 @@
-import { Component } from '@angular/core';
-import { RegistrationRequest } from '../../services/models';
-import { Router } from '@angular/router';
-import { AuthenticationService } from '../../services/services';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../../services/services/authentication.service';
+import {RegistrationRequest} from '../../services/models/registration-request';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
 
+  registerRequest: RegistrationRequest = {email: '', firstName: '', lastName: '', password: ''};
+  errorMsg: Array<string> = [];
+
   constructor(
-    private router:Router,
-    private authService:AuthenticationService
+    private router: Router,
+    private authService: AuthenticationService
   ) {
-
-  }
-
-  registrationRequest: RegistrationRequest = {email:'', firstName:'', lastName:'',
-    password:''
-  }
-
-  errorMessage: Array<string> = [];
-
-  register() {
-    this.errorMessage = [];
-    this.authService.register({
-      body: this.registrationRequest
-    }).subscribe({
-      next: ()=> {
-        this.router.navigate(['activate-account']);
-      },
-      error: (err)=> {
-        this.errorMessage = err.error.validationErrors;
-      }
-    })
   }
 
   login() {
     this.router.navigate(['login']);
   }
 
+  register() {
+    this.errorMsg = [];
+    this.authService.register({
+      body: this.registerRequest
+    })
+      .subscribe({
+        next: () => {
+          this.router.navigate(['activate-account']);
+        },
+        error: (err) => {
+          this.errorMsg = err.error.validationErrors;
+        }
+      });
+  }
 }
