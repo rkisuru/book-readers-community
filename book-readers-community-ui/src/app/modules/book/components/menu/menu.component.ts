916ccf675cd1ce24e041267375d0740f838fa86d
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-08-08 21:54:55 +0530

    feat: implement logout

--- book-readers-community-ui/src/app/modules/book/components/menu/menu.component.ts
+++ book-readers-community-ui/src/app/modules/book/components/menu/menu.component.ts
@@ -21,7 +21,8 @@ export class MenuComponent implements OnInit{
   }
 
   logout() {
-    throw new Error('Method not implemented.');
-}
+    localStorage.removeItem('token');
+    window.location.reload();
+  }
 
 }
