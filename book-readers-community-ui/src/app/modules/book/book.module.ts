f4a609cace2b4fb512bd21c49da9140c7ff9882b
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-08-08 21:54:15 +0530

    import components

--- book-readers-community-ui/src/app/modules/book/book.module.ts
+++ book-readers-community-ui/src/app/modules/book/book.module.ts
@@ -7,6 +7,11 @@ import { MenuComponent } from './components/menu/menu.component';
 import { BookListComponent } from './pages/book-list/book-list.component';
 import { BookCardComponent } from './components/book-card/book-card.component';
 import { RatingComponent } from './components/rating/rating.component';
+import { MyBooksComponent } from './pages/my-books/my-books.component';
+import { ManageBookComponent } from './pages/manage-book/manage-book.component';
+import {FormsModule} from "@angular/forms";
+import { BorrowedBookListComponent } from './pages/borrowed-book-list/borrowed-book-list.component';
+import {ReturnedBooksComponent} from "./pages/return-books/return-books.component";
 
 
 @NgModule({
@@ -15,11 +20,16 @@ import { RatingComponent } from './components/rating/rating.component';
     MenuComponent,
     BookListComponent,
     BookCardComponent,
-    RatingComponent
+    RatingComponent,
+    MyBooksComponent,
+    ManageBookComponent,
+    BorrowedBookListComponent,
+    ReturnedBooksComponent
   ],
   imports: [
     CommonModule,
-    BookRoutingModule
+    BookRoutingModule,
+    FormsModule
   ]
 })
 export class BookModule { }
