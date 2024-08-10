88884b408b04cbae4616d6464a044b033db49b20
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-08-10 10:57:23 +0530

    implement keycloak user authentication

--- book-readers-community-ui/src/app/app.module.ts
+++ book-readers-community-ui/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,6 +10,11 @@ import { RegisterComponent } from './pages/register/register.component';
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 import {CodeInputModule} from "angular-code-input";
 import { HttpTokenInterceptor } from './services/interceptor/http-token.interceptor';
+import {KeycloakService} from "./services/keycloak/keycloak.service";
+
+export function kcFactory(kcService: KeycloakService) {
+  return () => kcService.init();
+}
 
 @NgModule({
   declarations: [
@@ -31,7 +36,13 @@ import { HttpTokenInterceptor } from './services/interceptor/http-token.intercep
       provide: HTTP_INTERCEPTORS,
       useClass: HttpTokenInterceptor,
       multi: true
-     }],
+     },
+    {
+      provide: APP_INITIALIZER,
+      deps: [KeycloakService],
+      useFactory: kcFactory,
+      multi: true
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
