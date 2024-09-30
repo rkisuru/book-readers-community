510f599941e23ba0d0d0e6c38dc43e0b3c6dd76e
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-30 12:53:27 +0530

    rename directory

--- book-readers-community-api/src/main/java/com/rkisuru/book/config/ApplicationAuditAware.java
+++ book-readers-community-api/src/main/java/com/rkisuru/book/config/ApplicationAuditAware.java
@@ -1,7 +1,6 @@
 package com.rkisuru.book.config;
 
 
-import com.rkisuru.book.user.User;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
