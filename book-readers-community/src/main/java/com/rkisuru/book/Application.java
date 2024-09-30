8124a40912e8ebc6d06f60cabae9db40c23495b4
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-09-30 10:39:58 +0530

    remove code comment

--- book-readers-community/src/main/java/com/rkisuru/book/Application.java
+++ book-readers-community/src/main/java/com/rkisuru/book/Application.java
@@ -1,11 +1,7 @@
 package com.rkisuru.book;
 
-import com.rkisuru.book.role.Role;
-import com.rkisuru.book.role.RoleRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -18,14 +14,4 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	/*@Bean
-	public CommandLineRunner runner(RoleRepository roleRepository){
-		return args -> {
-			if(roleRepository.findByName("USER").isEmpty()){
-				roleRepository.save(
-						Role.builder().name("USER").build()
-				);
-			}
-		};
-	}*/
 }
