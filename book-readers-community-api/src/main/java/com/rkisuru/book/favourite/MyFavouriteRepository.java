171774d2c64e77662c1cbea4e226f5854ccbd1cc
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-10-03 01:53:10 +0530

    implement get all favourites by user

--- book-readers-community-api/src/main/java/com/rkisuru/book/favourite/MyFavouriteRepository.java
+++ book-readers-community-api/src/main/java/com/rkisuru/book/favourite/MyFavouriteRepository.java
@@ -1,13 +1,18 @@
 package com.rkisuru.book.favourite;
 
+import com.rkisuru.book.book.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MyFavouriteRepository extends JpaRepository<MyFavourite, Integer> {
 
     @Query("SELECT fav FROM MyFavourite fav WHERE fav.book.id = :bookId AND fav.createdBy = :connectedUser")
     Optional<MyFavourite> findByBookIdUserId(@Param("bookId") Integer bookId, @Param("connectedUser") String connectedUser);
+
+    @Query("SELECT favs FROM MyFavourite favs WHERE favs.createdBy = :connectedUser")
+    List<MyFavourite> findByConnectedUser(@Param("connectedUser") String connectedUser);
 }
