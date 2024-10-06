b58cedefa7f1ade29257d332b955a2079df750b9
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-10-06 13:15:50 +0530

    optimize code

--- book-readers-community-ui/src/app/app-routing.module.ts
+++ book-readers-community-ui/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 import {authGuard} from "./services/guard/auth.guard";
 
 const routes: Routes = [
@@ -15,17 +13,11 @@ const routes: Routes = [
     path:'login', component:LoginComponent
   },
   {
-    path:'register', component:RegisterComponent
-  },
-  {
-    path:'activate-account', component: ActivateAccountComponent
-  },
-  {
     path: 'books', loadChildren: ()=> import('./modules/book/book.module').then(
       m=> m.BookModule
     ),
     canActivate: [authGuard]
-  }
+  },
 
 ];
 
--- book-readers-community-ui/src/app/app.module.ts
+++ book-readers-community-ui/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppComponent } from './app.component';
 import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from "@angular/common/http";
 import { LoginComponent } from './pages/login/login.component';
 import { FormsModule } from '@angular/forms';
-/*import { RegisterComponent } from './pages/register/register.component';
-import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';*/
 import {CodeInputModule} from "angular-code-input";
 import { HttpTokenInterceptor } from './services/interceptor/http-token.interceptor';
 import {KeycloakService} from "./services/keycloak/keycloak.service";
@@ -20,9 +18,7 @@ export function kcFactory(kcService: KeycloakService) {
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    /*RegisterComponent,
-    ActivateAccountComponent,*/
+    LoginComponent
   ],
     imports: [
       BrowserModule,
