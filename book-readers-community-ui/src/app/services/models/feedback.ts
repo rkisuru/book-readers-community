988da0c794c8778f6a8a7ad69d24eaa19998f1ea
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-10-06 13:16:50 +0530

    implement feedback model

---
+++ book-readers-community-ui/src/app/services/models/feedback.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export interface Feedback {
+  comment?: string;
+  createdAt?: string;
+  createdBy?: string;
+  get_user?: string;
+  id?: number;
+  lastModifiedAt?: string;
+  lastModifiedBy?: string;
+  note?: number;
+}
