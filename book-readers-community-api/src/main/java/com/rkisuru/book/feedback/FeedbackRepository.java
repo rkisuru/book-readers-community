1bf610ae09ada0bb5479c6f86c786b22d146d166
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-10-06 13:07:54 +0530

    refactor: findAllByBookId()

--- book-readers-community-api/src/main/java/com/rkisuru/book/feedback/FeedbackRepository.java
+++ book-readers-community-api/src/main/java/com/rkisuru/book/feedback/FeedbackRepository.java
@@ -1,16 +1,13 @@
 package com.rkisuru.book.feedback;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface FeedbackRepository extends JpaRepository<Feedback, Integer> {
-    @Query("""
-    SELECT feedback
-    FROM Feedback feedback
-    WHERE feedback.book.id = :bookId
-    """)
-    Page<Feedback> findAllByBookId(@Param("bookId") Integer bookId, Pageable pageable);
+
+    @Query("SELECT feedbacks FROM Feedback feedbacks WHERE feedbacks.book.id = :bookId")
+    List<Feedback> findAllByBookId(@Param("bookId") Integer bookId);
 }
